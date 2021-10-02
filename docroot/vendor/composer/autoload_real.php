905b42b0ec9cf447f11ef3336cc5d9c1b839196f
Richie Redizm <richieredizm@gmail.com>
2021-10-02 16:06:09 +0100

    update to drupal9 with composer

--- docroot/vendor/composer/autoload_real.php
+++ docroot/vendor/composer/autoload_real.php
@@ -22,8 +22,10 @@ class ComposerAutoloaderInitf197092365111500249304d9be7b237b
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitf197092365111500249304d9be7b237b', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitf197092365111500249304d9be7b237b', 'loadClassLoader'));
 
         $includePaths = require __DIR__ . '/include_paths.php';
@@ -32,7 +34,7 @@ class ComposerAutoloaderInitf197092365111500249304d9be7b237b
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitf197092365111500249304d9be7b237b::getInitializer($loader));
         } else {
